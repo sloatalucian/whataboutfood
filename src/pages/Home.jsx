db1634b3289cc536b78d62b31d9209682b3fc1a2
sloatalucian <sloatalucian@yahoo.com>
2026-05-18 18:33:20 +0300

    fix: eliminat HighlightText duplicat

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -35,21 +35,6 @@ const ORASE = [
   "Deva",
 ];
 
-function HighlightText({ text, query }) {
-  if (!query) return <span>{text}</span>;
-  const idx = text.toLowerCase().indexOf(query.toLowerCase());
-  if (idx === -1) return <span>{text}</span>;
-  return (
-    <span>
-      {text.slice(0, idx)}
-      <span style={{ color: "#e07a47", fontWeight: 800 }}>
-        {text.slice(idx, idx + query.length)}
-      </span>
-      {text.slice(idx + query.length)}
-    </span>
-  );
-}
-
 function SearchBar({
   onSelect,
   selectedCity,
