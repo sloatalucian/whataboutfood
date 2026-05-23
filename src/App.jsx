b6e202b89c2ff9cb2fd7763d1faf503af01c1439
sloatalucian <sloatalucian@yahoo.com>
2026-05-23 17:19:07 +0300

    fix: FAB pozitionat corect + sheet full-width

--- src/App.jsx
+++ src/App.jsx
@@ -870,10 +870,9 @@ function Router() {
                 style={{
                   position: "fixed",
                   bottom: 0,
-                  left: "50%",
-                  transform: "translateX(-50%)",
+                  left: 0,
+                  right: 0,
                   width: "100%",
-                  maxWidth: 430,
                   background: "#111009",
                   borderRadius: "22px 22px 0 0",
                   borderTop: "1px solid #2a2218",
@@ -1056,7 +1055,7 @@ function Router() {
               style={{
                 position: "fixed",
                 bottom: 86,
-                left: 20,
+                left: "max(20px, calc(50% - 195px))",
                 width: 54,
                 height: 54,
                 borderRadius: "50%",
